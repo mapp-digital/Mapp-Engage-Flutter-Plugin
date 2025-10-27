916415c2ca7eec88150c7f50313db4066913817c
tafilovic <sekitafilovic@gmail.com>
2025-10-27 14:14:32 +0100

    updated test app

--- example/lib/home_page.dart
+++ example/lib/home_page.dart
@@ -59,10 +59,11 @@ class _HomePageState extends State<HomePage> {
         "",
         appConfig.server,
         appConfig.appID,
-        appConfig.tenantID,);
+        appConfig.tenantID,
+        NotificationMode.backgroundOnly);
     await initPlatformState();
     await requestPermissionPostNotifications();
-    await MappSdk.showNotificationsOnForeground(true);
+    //await MappSdk.showNotificationsOnForeground(true);
   }
 
   void handledRemoteNotification(dynamic arguments) {
